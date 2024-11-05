3007b83aa7b8704c7a14124c4abdcd7ee9f7eb37
isarail42 <isarail4242@gmail.com>
2024-11-05 19:26:06 +0530

    Initial commit with new folder

---
+++ Contracts-Deploy/src/utils.js
@@ -0,0 +1,30 @@
+require('colors');
+const fs = require('fs');
+const readlineSync = require('readline-sync');
+
+function loadNetworkConfig(networkType) {
+  const filePath = `./chains/${networkType}.json`;
+  try {
+    const rawData = fs.readFileSync(filePath);
+    return JSON.parse(rawData);
+  } catch (error) {
+    console.error(`Error loading network configuration: ${error.message}`.red);
+    process.exit(1);
+  }
+}
+
+function getUserInput() {
+  const name = readlineSync.question('Enter token name: '.cyan);
+  const symbol = readlineSync.question('Enter token symbol: '.cyan);
+  const supply = readlineSync.question('Enter token supply: '.cyan);
+  return { name, symbol, supply };
+}
+
+function displayHeader() {
+  process.stdout.write('\x1Bc');
+
+}
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+module.exports = { loadNetworkConfig, getUserInput, displayHeader, delay };
